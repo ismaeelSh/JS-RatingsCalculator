0a9cfc45b273e5de4604555834488f0fd0b55d96
Ismaeel Shuaib Alfani <39034490+ismaeelSh@users.noreply.github.com>
2019-11-02 04:16:15 +0000

    update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -1,5 +1,5 @@
 const collect_ratings = () => {
-    const ratings = { 'count':0, 'average':0 };
+    const ratings = { 'count':0, 'sum':0, 'average':0 };
 
     let rating = 0;
 
@@ -7,7 +7,7 @@ const collect_ratings = () => {
     elements.forEach(element => {
         rating = parseInt(element.id.replace('star', ''));
         ratings.count += parseInt(element.value);
-        parseInt.sum += parseInt(element.value) * rating;
+        ratings.sum += parseInt(element.value) * rating;
     });
 
     if(ratings.count !==0 ){
@@ -19,5 +19,5 @@ const collect_ratings = () => {
 
 document.addEventListener('change', () => {
     const ratings = collect_ratings();
-    document.querySelector('#average').value = ratings.average.forFixed(2);
+    document.querySelector('#average').value = ratings.average.toFixed(2);
 });
